2430e20e2a26dd85772f0c976c88c736657517ac
alex.fan <alex.fan@okchem.com>
2019-03-26 13:50:58 +0800

    basic layout

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,70 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
+import GridLayout from 'react-grid-layout'
 
-class App extends Component {
-  render() {
-    return <div className="App">Hello World</div>;
-  }
-}
+/*
+  a: 平台流量数据(折线图)
+  b: 有效用户数
+  c: 注册用户数
+  d: 付费用户数
 
-export default App;
+  e: 营收金额 (饼图)
+  f: GMV 营收
+*/
+const layout = [
+  {
+    i: 'a',
+    x: 0,
+    y: 0,
+    w: 4,
+    h: 12,
+  },
+  {
+    i: 'b',
+    x: 4,
+    y: 0,
+    w: 2,
+    h: 4,
+  },
+  {
+    i: 'c',
+    x: 4,
+    y: 0,
+    w: 2,
+    h: 4,
+  },
+  {
+    i: 'd',
+    x: 4,
+    y: 0,
+    w: 2,
+    h: 4,
+  },
+  {
+    i: 'e',
+    x: 6,
+    y: 0,
+    w: 6,
+    h: 8,
+  },
+  {
+    i: 'f',
+    x: 6,
+    y: 0,
+    w: 6,
+    h: 4,
+  },
+]
+
+const App = () => (
+  <GridLayout className="layout" layout={layout} cols={12} rowHeight={60} width={1920}>
+    <div key="a">平台流量数据(折线图)</div>
+    <div key="b">有效用户数</div>
+    <div key="c">注册用户数</div>
+    <div key="d">付费用户数</div>
+    <div key="e">营收金额 (饼图)</div>
+    <div key="f">GMV 营收</div>
+  </GridLayout>
+)
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// grid layout dependencies css
+import '../node_modules/react-grid-layout/css/styles.css'
+import '../node_modules/react-resizable/css/styles.css'
+import './index.css'
+import App from './App/App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
