e4391e6005a109db764b0058ec1964b4867b8ad8
alex.fan <alex.fan@okchem.com>
2019-03-26 15:10:28 +0800

    style fix

--- src/Components/Charts/PvChart/PvChart.js
+++ src/Components/Charts/PvChart/PvChart.js
@@ -51,28 +51,26 @@ const cols = {
 }
 
 const PvChart = () => (
-  <div>
-    <Chart height={400} data={data} scale={cols} forceFit>
-      <Axis name="year" />
-      <Axis name="value" />
-      <Tooltip
-        crosshairs={{
-          type: 'y',
-        }}
-      />
-      <Geom type="line" position="year*value" size={2} />
-      <Geom
-        type="point"
-        position="year*value"
-        size={4}
-        shape="circle"
-        style={{
-          stroke: '#fff',
-          lineWidth: 1,
-        }}
-      />
-    </Chart>
-  </div>
+  <Chart height={400} data={data} scale={cols} forceFit>
+    <Axis name="year" />
+    <Axis name="value" />
+    <Tooltip
+      crosshairs={{
+        type: 'y',
+      }}
+    />
+    <Geom type="line" position="year*value" size={2} />
+    <Geom
+      type="point"
+      position="year*value"
+      size={4}
+      shape="circle"
+      style={{
+        stroke: '#fff',
+        lineWidth: 1,
+      }}
+    />
+  </Chart>
 )
 
 export default PvChart
