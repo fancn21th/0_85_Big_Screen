36f95c568c53b0c500b3c356abf6d767f9244545
alex.fan <alex.fan@okchem.com>
2019-03-26 15:41:12 +0800

    theme file added

--- src/Components/Charts/IncomeChart/IncomeChart.js
+++ src/Components/Charts/IncomeChart/IncomeChart.js
@@ -1,93 +1,124 @@
+// data-set 可以按需引入，除此之外不要引入别的包
 import React from 'react'
 import {
-  Chart, Geom, Axis, Tooltip, Coord, Label, Legend,
+  Chart, Axis, Coord, Geom, Guide, View,
 } from 'bizcharts'
-import DataSet from '@antv/data-set'
 
-class Innerlabel extends React.Component {
-  render() {
-    const { DataView } = DataSet
-    const data = [
-      {
-        item: '事例一',
-        count: 40,
-      },
-      {
-        item: '事例二',
-        count: 21,
-      },
-      {
-        item: '事例三',
-        count: 17,
-      },
-      {
-        item: '事例四',
-        count: 13,
-      },
-      {
-        item: '事例五',
-        count: 9,
-      },
-    ]
-    const dv = new DataView()
-    dv.source(data).transform({
-      type: 'percent',
-      field: 'count',
-      dimension: 'item',
-      as: 'percent',
-    })
-    const cols = {
-      percent: {
-        formatter: (val) => {
-          val = `${val * 100}%`
-          return val
-        },
-      },
-    }
-    return (
-      <div>
-        <Chart height={400} data={dv} scale={cols} padding={[80, 100, 80, 80]} forceFit>
-          <Coord type="theta" radius={0.75} />
-          <Axis name="percent" />
-          <Legend position="right" offsetY={-window.innerHeight / 2 + 120} offsetX={-100} />
-          <Tooltip
-            showTitle={false}
-            itemTpl='<li><span style="background-color:{color};" class="g2-tooltip-marker"></span>{name}: {value}</li>'
-          />
-          <Geom
-            type="intervalStack"
-            position="percent"
-            color="item"
-            tooltip={[
-              'item*percent',
-              (item, percent) => {
-                percent = `${percent * 100}%`
-                return {
-                  name: item,
-                  value: percent,
-                }
-              },
-            ]}
-            style={{
-              lineWidth: 1,
-              stroke: '#fff',
-            }}
-          >
-            <Label
-              content="percent"
-              offset={-40}
-              textStyle={{
-                rotate: 0,
-                textAlign: 'center',
-                shadowBlur: 2,
-                shadowColor: 'rgba(0, 0, 0, .45)',
-              }}
-            />
-          </Geom>
-        </Chart>
-      </div>
-    )
-  }
+const { Text } = Guide
+
+// 下面的代码会被作为 cdn script 注入 注释勿删
+// CDN START
+// 构造数据
+const data1 = []
+for (let i = 0; i < 50; i += 1) {
+  const item = {}
+  item.type = `${i}`
+  item.value = 10
+  data1.push(item)
 }
 
-export default Innerlabel
+const data2 = []
+for (let i = 0; i < 50; i += 1) {
+  const item = {}
+  item.type = `${i}`
+  item.value = 10
+  if (i === 25) {
+    item.value = 14
+  }
+  if (i > 25) {
+    item.value = 0
+  }
+  data2.push(item)
+}
+
+const cols = {
+  type: {
+    range: [0, 1],
+  },
+  value: {
+    sync: true,
+  },
+}
+
+const colsView2 = {
+  type: {
+    tickCount: 3,
+  },
+}
+
+const IncomeChart = () => (
+  <Chart height={500} data={[1]} scale={cols} padding={[0, 0, 200, 0]} forceFit>
+    <View data={data1}>
+      <Coord
+        type="polar"
+        startAngle={(-9 / 8) * Math.PI}
+        endAngle={(1 / 8) * Math.PI}
+        radius={0.8}
+        innerRadius={0.75}
+      />
+      <Geom type="interval" position="type*value" color="rgba(0, 0, 0, 0.09)" size={6} />
+    </View>
+    <View data={data1} scale={colsView2}>
+      <Coord
+        type="polar"
+        startAngle={(-9 / 8) * Math.PI}
+        endAngle={(1 / 8) * Math.PI}
+        radius={0.55}
+        innerRadius={0.95}
+      />
+      <Geom type="interval" position="type*value" color="rgba(0, 0, 0, 0.09)" size={6} />
+      <Axis
+        name="type"
+        grid={null}
+        line={null}
+        tickLine={null}
+        label={{
+          offset: -20,
+          textStyle: {
+            fontSize: 18,
+            fill: '#CBCBCB',
+            textAlign: 'center',
+          },
+          formatter: (val) => {
+            if (val === '49') {
+              return 50
+            }
+            return val
+          },
+        }}
+      />
+      <Axis name="value" visible={false} />
+    </View>
+    <View data={data2}>
+      <Coord
+        type="polar"
+        startAngle={(-9 / 8) * Math.PI}
+        endAngle={(1 / 8) * Math.PI}
+        radius={0.8}
+        innerRadius={0.75}
+      />
+      <Geom
+        type="interval"
+        position="type*value"
+        color={['value', '#3023AE-#53A0FD']}
+        opacity={1}
+        size={6}
+      />
+      <Guide>
+        <Text
+          position={['50%', '65%']}
+          content="26%"
+          style={{
+            fill: '#262626',
+            fontSize: 64,
+            textAlign: 'center',
+            textBaseline: 'middle',
+          }}
+        />
+      </Guide>
+    </View>
+  </Chart>
+)
+
+// CDN END
+export default IncomeChart
