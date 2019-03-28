1597e1900f3aae2446e1453c6bdbd43c220f60ce
alex.fan <alex.fan@okchem.com>
2019-03-28 23:17:21 +0800

    presentage

--- src/Components/Income/Income.js
+++ src/Components/Income/Income.js
@@ -5,18 +5,22 @@ import Figure from '../Texts/Figure/Figure1'
 import IncomeChart from '../Charts/IncomeChart/IncomeChart'
 import './Income.css'
 
-const Income = ({ transfer, contract }) => (
-  <div className="income-container">
-    <div className="income-title">
-      <Title className="income-title--top" title="营收金额" subTitle="2019" />
-      <div className="income-title--bottom">
-        <Figure className="income-title--bottom__half" title="已汇款金额" figure={transfer} />
-        <Figure className="income-title--bottom__half" title="合同总金额" figure={contract} />
+const Income = ({ transfer, contract }) => {
+  const presentage = 60
+
+  return (
+    <div className="income-container">
+      <div className="income-title">
+        <Title className="income-title--top" title="营收金额" subTitle="2019" />
+        <div className="income-title--bottom">
+          <Figure className="income-title--bottom__half" title="已汇款金额" figure={transfer} />
+          <Figure className="income-title--bottom__half" title="合同总金额" figure={contract} />
+        </div>
       </div>
+      <IncomeChart presentage={presentage} />
     </div>
-    <IncomeChart />
-  </div>
-)
+  )
+}
 
 Income.propTypes = {
   transfer: PropTypes.string.isRequired,
